2a9a0ca3c82c0257436c275ddc0917844285bb6a
yaseregko <ya.seregko@gmail.com>
2021-09-21 00:36:52 +0300

    fix: Добавлен сенсор с именем последнего снапшота

--- smtp2mqtt.js
+++ smtp2mqtt.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const deviceId      = '18483494' // Uniq device id, change it!!!
 const fs            = require('fs');
 const {SMTPServer}  = require('smtp-server');
 const simpleParser  = require('mailparser').simpleParser;
 const mqtt          = require('mqtt');
 const config        = JSON.parse(fs.readFileSync('/data/options.json', 'utf8'));
+const deviceId      = config.deviceId || '0';
 const mqttUrl       = config.mqtt_url || 'mqtt://core-mosquitto:1883';
 const mqttOptions   = {
                        clientId: config.mqtt_clientId || 'smtp2mqtt',
@@ -17,14 +17,11 @@ const smtpHost      = config.smtp_host || '0.0.0.0';
 //const smtp_username      = config.smtp_username || 'username';
 //const smtp_password      = config.smtp_password || 'password';
 const mediaPath     = config.media_path || '/media';
-const networkInfo   = { "mac": "00:00:00"
+const deviceInfo    = { "name": "Yoosee Doorbell SD-05",                                    
+                        "manufacturer": "Yoosee", 
+                        "model": "SD-05",
+                        "sw_version": "13.0.5"
                       };
-const deviceInfo     = { "name": "Yoosee Doorbell SD-05",                                    
-                         "manufacturer": "Yoosee", 
-                         "model": "SD-05",
-                         "identifiers": deviceId,
-                         "sw_version": "13.0.5"
-                       };
 
 const smtp = new SMTPServer({
     secure: false,
@@ -45,18 +42,24 @@ smtp.listen(smtpPort, smtpHost, () => {
     mqttClient.on('connect', function () {
         var doorbellButton = {
                               "name": "Doorbell Button",
-                              "unique_id": "doorbell_" + deviceId,
                               "device": deviceInfo,
-                              "state_topic": "smtp2mqtt/binary_sensor/doorbell/" + deviceId + "/state",
+                              "state_topic": "smtp2mqtt/doorbell/" + deviceId + "/state",
                               "off_dly": "5",
-                              "pl_on": "bell",
-                              "pl_off": "idle"
+                              "pl_on": "BELL",
+                              "pl_off": "IDLE",
+                              "value_template": "{{ value_json.state }}"
                               };
         mqttClient.publish('homeassistant/binary_sensor/doorbell/' + deviceId + '/config', JSON.stringify(doorbellButton), { qos:0, retain: true });
+        var doorbellFilename = {
+                              "name": "Doorbell Snapshot Filename",
+                              "device": deviceInfo,
+                              "state_topic": "smtp2mqtt/doorbell/" + deviceId + "/state",
+                              "value_template": "{{ value_json.filename }}"
+                              };
+        mqttClient.publish('homeassistant/sensor/doorbell/' + deviceId + '/config', JSON.stringify(doorbellFilename), { qos:0, retain: true });
         var doorbellCamera = {
-                              "name": "Doorbell Camera Capture",
-                              "topic": "smtp2mqtt/camera/doorbell/" + deviceId + "/capture",
-                              "unique_id": "doorbell_" + deviceId,
+                              "name": "Doorbell Camera Snapshot",
+                              "topic": "smtp2mqtt/doorbell/" + deviceId + "/snapshot",
                               "device": deviceInfo
                               };
         mqttClient.publish('homeassistant/camera/doorbell/' + deviceId + '/config', JSON.stringify(doorbellCamera), { qos: 0, retain: true });
@@ -72,18 +75,21 @@ function onData(stream, session, callback) {
         for (let i in mail_object.attachments) {
             let attachment = mail_object.attachments[i];
             if(attachment.size !== 0){
-                let data = attachment.content;
                 let fileName = mediaPath + '/' + attachment.filename;
-                fs.writeFile(fileName, data, function(error) {
+                fs.writeFile(fileName, attachment.content, function(error) {
                     if(error) console.log('An error occurred:', error);
                     //callback(new Error(`Writing file with error: ${error}`));
                     console.log("Somebody bell in door. Photo saved in file:", fileName);
                 });
-                var buff = Buffer.from(data, 'utf-8');
+                var mqttSensorData = {
+                                  "state": "BELL",
+                                  "filename": attachment.filename
+                                  };
+                var mqttCameraData = Buffer.from(attachment.content, 'utf-8'); // В буфере изображение 
                 let mqttClient = mqtt.connect(mqttUrl, mqttOptions);
                 mqttClient.on('connect', function() {
-                    mqttClient.publish('smtp2mqtt/binary_sensor/doorbell/' + deviceId + '/state', 'bell', { qos: 0 });
-                    mqttClient.publish('smtp2mqtt/camera/doorbell/' + deviceId + '/capture', buff, { qos: 0 });
+                    mqttClient.publish('smtp2mqtt/doorbell/' + deviceId + '/state', JSON.stringify(mqttSensorData), { qos: 0 });
+                    mqttClient.publish('smtp2mqtt/doorbell/' + deviceId + '/snapshot', mqttCameraData, { qos: 0 });
                     mqttClient.end();
                 });
             }
